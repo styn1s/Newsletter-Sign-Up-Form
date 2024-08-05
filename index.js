95d8be65ac4fe062568c3b1f8398b46837dcd2c1
Maksim Yavorets <styn1sss@gmail.com>
2024-08-05 16:57:15 +0300

    Refactored main page code

--- index.js
+++ index.js
@@ -2,16 +2,32 @@ const form = document.querySelector("form");
 const emailInput = document.getElementById("email-input");
 const emailError = document.getElementById("error-text");
 
-emailInput.addEventListener("input", (event) => {
-  if (emailInput.validity.typeMismatch) {
-    showError();
-  } else {
-    hideError();
-  }
-});
+function showError() {
+  emailError.textContent = "Valid email required!";
+  emailInput.style.borderColor = "rgba(255,85,114,255)";
+  emailInput.style.color = "rgba(255,85,114,255)";
+  emailInput.style.backgroundColor = "rgba(255,85,114,0.1)";
+}
 
-form.addEventListener("submit", function (event) {
-  event.preventDefault(); // Prevent default form submission
+function hideError() {
+  emailError.textContent = "";
+  emailInput.style.borderColor = "lightgray";
+  emailInput.style.color = "black";
+  emailInput.style.backgroundColor = "white";
+}
+
+function updateImg() {
+  const imgElement = document.querySelector("img");
+  if (window.innerWidth <= 625) {
+    imgElement.src = "./assets/images/illustration-sign-up-mobile.svg";
+  } else {
+    imgElement.src = "./assets/images/illustration-sign-up-desktop.svg";
+  }
+}
+
+function onSubmitForm(event) {
+  // Prevent default form submission
+  event.preventDefault();
 
   // Get form data
   const formData = new FormData(this); 
@@ -30,28 +46,16 @@ form.addEventListener("submit", function (event) {
 
   // Submit the form
   this.submit();
-});
-
-window.addEventListener("resize", function () {
-  if (window.innerWidth <= 625) {
-    document.querySelector("img").src =
-      "./assets/images/illustration-sign-up-mobile.svg";
-  } else {
-    document.querySelector("img").src =
-      "./assets/images/illustration-sign-up-desktop.svg";
-  }
-});
-
-function showError() {
-  emailError.textContent = "Valid email required!";
-  emailInput.style.borderColor = "rgba(255,85,114,255)";
-  emailInput.style.color = "rgba(255,85,114,255)";
-  emailInput.style.backgroundColor = "rgba(255,85,114,0.1)";
 }
 
-function hideError() {
-  emailError.textContent = "";
-  emailInput.style.borderColor = "lightgray";
-  emailInput.style.color = "black";
-  emailInput.style.backgroundColor = "white";
-}
+document.addEventListener("DOMContentLoaded", function() {
+  window.addEventListener("resize", updateImg);
+  form.addEventListener("submit", onSubmitForm);
+  emailInput.addEventListener("input", (event) => {
+    if (emailInput.validity.typeMismatch) {
+      showError();
+    } else {
+      hideError();
+    }
+  });
+})
